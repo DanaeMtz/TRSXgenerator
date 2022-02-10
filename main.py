f605508effe9a929d089bfba0e0ddde7aacf4a31
DanaeMtz <martinezvd1992@gmail.com>
2022-02-09 20:02:25 -0500

    modify gitignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from buildtrsx.build_dictionaries.build_dict import trsx_dictionary
 from buildtrsx.build_project import trsx_project_node, trsx_metadata_node, trsx_sources_node
+from buildtrsx.build_dictionaries.build_dict import trsx_dictionary
 from buildtrsx.build_ontology.build_ontology import trsx_links_node, trsx_intents_node, trsx_relations_node, trsx_settings_node, trsx_entities_node
 
 #---# ACCOUNT TYPES #---#
@@ -45,11 +45,12 @@ entity_relations_types = {'ACCOUNT_TYPE': {'entity_type':'List'}
                         , 'BANK_ACCOUNT': {'entity_type':'Relationship', 'type':"hasA", 'conceptref': ['ACCOUNT_BALANCE', 'ACCOUNT_NUMBER', 'ACCOUNT_TYPE']}
                         }
 
+
 #print(entity_types['ACCOUNT_TYPE']['entity_type'])
 #print(trsx_relations_node(rel_dict={'entity_type':'Relationship', 'type':"hasA", 'conceptref': ['ACCOUNT_BALANCE', 'ACCOUNT_NUMBER', 'ACCOUNT_TYPE']}))
 #print(trsx_relations_node(rel_dict={'entity_type':'Relationship', 'type':"isA", 'conceptref':"nuance_AMOUNT"}))
 #print(trsx_settings_node())
-print(trsx_entities_node(rel_typ_dict = entity_relations_types))
+#print(trsx_entities_node(rel_typ_dict = entity_relations_types))
 #print(entity_relationships['BANK_ACCOUNT'])
 #{'type':"isA", 'conceptref':"ACCOUNT_TYPE"}
 
@@ -57,24 +58,36 @@ print(trsx_entities_node(rel_typ_dict = entity_relations_types))
 #print(entity_relationships['FROM_ACCOUNT']['conceptref'])
 #print(trsx_relations_node(type = entity_relationships['FROM_ACCOUNT']['type']
 #                          , conceptref = entity_relationships['FROM_ACCOUNT']['conceptref']))
-print(trsx_intents_node(int_ent_dict = ontology_investment))
+#print(trsx_intents_node(int_ent_dict = ontology_investment))
 #print(trsx_dictionary(dictionary = ACCOUNT_TYPE))
-#print(trsx_project_node())
 #print(trsx_metadata_node(str="str"))
 #print(trsx_sources_node())
 #print(trsx_dictionary(dictionary = Annuities_retirement_funds_FR))
 #print(trsx_dictionary(dictionary = regular_accounts_FR))
 
-#def trsx_dictionary(dictionary):
-#    doc, tag, text = Doc().tagtext()
-#    for key in dictionary:
-#        for value in dictionary[key]:
-#            #print(key, 'corresponds to', value)
-#            doc.stag('entry', literal=value, value=key)
-#
-#    return doc.getvalue()
+# verified
+
+# build project script
+metadata = trsx_metadata_node(author = "Danae Martinez",
+                              version = "1.0.0",
+                              description = "my NLU model",
+                              date = "january 2022")
+# sources #
+dict1 = {'name':"IOT_Domain", 'type':"CUSTOM"}
+dict2 = {"name":"DTV_Domain", 'uri':"http://localhost:80/my_local_dtv_domain",  'version':"1.0",  'type':"PREBUILT"}
+dict3 = {'name':"NCSRef_Rejection", 'uri':"http://localhost:80/ncs_ref_rejection_model", 'version':"1.0", 'type':"REJECTION"}
+sources = {'source_1':dict1,
+           'source_2':dict2,
+           'source_3':dict3}
+
+project_attributes = {'xmlns:nuance':'https://developer.nuance.com/mix/nlu/trsx'
+                    , 'xml:lang':'eng-USA' # 'fra-CAN'
+                    , 'nuance:enginePackVersion':'hosted'}
+sources = trsx_sources_node(sources = sources)
+
+print(trsx_project_node(attributes=project_attributes, metadata_node = metadata, sources_node=sources))
+
 
 #if __name__ == '__main__':
-#    print(trsx_dictionary(dictionary = registered_plans_FR))
 
 
